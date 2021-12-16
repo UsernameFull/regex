ff2f50658bf4f4686dcd915cf9f0fc34f528a26e
UsernameFull <tohowtodoit@gmail.com>
2021-12-17 00:55:30 +0800

    seems success

--- src/enfa2nfa.ts
+++ src/enfa2nfa.ts
@@ -9,11 +9,14 @@ class NFAState {
 }
 
 function trasE_NFA2NFA(enfa: E_NFA) {
-  // 当前状态所有ε相连的节点是同一类节点，均可加入current,此处采用bfs
-  function neighbourState(current: Set<E_NFAState>) {
-    //bfs算法中的队列
-    let queue = [...current];
-    //已经访问过的节点集合
+  //
+  let id = 0;
+
+  // 返回当前状态所有ε相连的节点是同一类节点，均可加入current,此处采用bfs
+  function neighbourState(current: E_NFAState) {
+    // bfs算法中的队列
+    let queue = [current];
+    // 已经访问过的节点集合
     let visted: Set<E_NFAState> = new Set();
     visted.add(queue[0]);
     while (queue.length != 0) {
@@ -26,12 +29,55 @@ function trasE_NFA2NFA(enfa: E_NFA) {
       });
     }
     // 此时visted中已经包含所有与输入节点集合通过ε相连的*所有节点*
-    // 我们需要筛选出有一般连接的节点并返回这类的集合
-    return new Set([...visted].filter((e)=>!isEmpty(e.Next)));
+    return visted;
+  }
+  
+  // 将
+  function buildNFAState(states:Set<E_NFAState>,map:Map<E_NFAState,NFAState>) {
+    let tmp = new NFAState()
+    tmp.id = id;
+    id++
+    states.forEach((e)=>{
+      // 该节点在映射map内，则先将其加入映射map
+      if(!map.has(e)){
+        map.set(e,tmp)
+      }
+      // 存在一般连接时，需要特殊对待
+      if(!isEmpty(e.Next)){
+        let symbol = Object.keys(e.Next)[0];
+        tmp.Next[symbol] = new Set();
 
+        if(map.has(e.Next[symbol])){
+          tmp.Next[symbol].add(map.get(e.Next[symbol]) as NFAState)
+        }else{
+          let currentStates: Set<E_NFAState> = neighbourState(e.Next[symbol]);
+          tmp.Next[symbol].add(buildNFAState(currentStates,map)) //************这里还有大问题****** */
+        }
+      }
+      // 如果相邻节点为终点，则构造的新节点也应为终点
+      if(e.isEnd){
+        tmp.isEnd = true;
+      }
+    })
+    return tmp
   }
 
-  function dfs(enfa: E_NFA) {}
-
-  trasE_NFA2NFA(enfa);
+  function dfs(eNfaState:E_NFAState ):NFAState {
+    let enfa2nfaMap = new Map();
+    let currentStates: Set<E_NFAState> = neighbourState(eNfaState);
+    let root = buildNFAState(currentStates,enfa2nfaMap);
+    // for (const symbol in root.Next) {
+    //   root.Next[symbol].forEach(e=>{
+    //     e = dfs(e)
+    //   })
+    // }
+    return root
+  }
+  return dfs(enfa.start)
 }
+
+
+let a = creatRegexE_NFA()
+let b = trasE_NFA2NFA(a)
+
+console.log(b)
